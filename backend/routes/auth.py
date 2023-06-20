7d124daf14f69eae318b64d2025d0a1abd3f9e43
Anthony <z5320020@ad.unsw.edu.au>
2023-06-20 13:30:28 +1000

    Tweak: moved user_register.py into db folder

--- backend/routes/auth.py
+++ backend/routes/auth.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, request, jsonify
-from backend.user_register import user_register
+from backend.db.user_register import user_register
 from backend.db.db import get_database
 from backend.helpers import is_valid_email
 
