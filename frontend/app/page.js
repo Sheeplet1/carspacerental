749f528582a67d337f40dbcaf85e7b49ecf25be7
marianyuzu <marianyuzuninomi@gmail.com>
2023-06-21 18:23:48 +1000

    write a very rough landing page

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -1,9 +1,23 @@
 import React from 'react'
+import Selectbar from '@components/Selectbar'
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
+return (
+  <div className="flex flex-row h-screen">
+    <Selectbar />
+    <div className="flex flex-col mr-44 gap-16">
+      <p className="logo_text">ACCONPANY YOUR</p>
+      <p className="logo_text">JOURNEY WITH COMFORT</p>
+      <label className="text-xs text-gray-400">Car rent services for various terrain with guaranteed qualitys</label>
+
+      <div className="flex justify-start">
+        <button className="blue_btn">
+          Learn More
+        </button>
+      </div>
+    </div>
+  </div>
+   )
+ }
 
 export default Home
