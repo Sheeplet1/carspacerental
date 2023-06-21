d1ea064418b12565d6aebaa2d2e1391c74337e11
Careen Hakim <careenhakim@Careens-MacBook-Pro.local>
2023-06-21 16:36:05 +1000

    Updated forgetpassword link on login page and Updated work diary

--- frontend/app/login/page.js
+++ frontend/app/login/page.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Sidebar from '@components/Sidebar'
+import Link from 'next/link'
 
 const Login = () => {
   return (
@@ -8,21 +9,27 @@ const Login = () => {
       <div className='flex flex-col mr-44'>
         <p className='heading_text'>Login</p>
 
-        <label className='mb-2'>Email-Address:</label>
+        <label className='mb-2 mt-4'>Email-Address:</label>
         <input className='w-96 border-2 border-gray-300 rounded-3xl p-2 mb-14' type='email' placeholder='Email' />
 
         <label className='mb-2'>Password:</label>
         <input className='w-96 border-2 border-gray-300 rounded-3xl p-2 mb-14' type='password' placeholder='Password' />
 
-        <button className='mb-10 text-teal-400 text-xs align-content: flex-start'>Forgot password?</button>
+        <Link href='/forgotpassword'>
+        <button className='mb-8 text-teal-400 text-xs align-content: flex-start'>Forgot password?</button>
+        </Link>
 
         <div className='flex justify-between'>
-          <button className='blue_btn'>
-            Back
-          </button>
-          <button className='blue_btn'>
-            Next
-          </button>
+          {/* <Link href='/'> */}
+            <button className='blue_btn'>
+              Back
+            </button>
+          {/* </Link>
+          <Link> */}
+            <button className='blue_btn'>
+              Next
+            </button>
+          {/* </Link> */}
         </div>
       </div>
     </div>
