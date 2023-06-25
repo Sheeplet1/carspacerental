a9b84b0b1ae6e469af37f3c5f4afbb82eb0c05d6
Mani Singal <mani88singal@gmail.com>
2023-06-25 22:49:31 +1000

    Modified fetch request and enables cors on the backend

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,14 @@
 from flask import Flask
 from flask_jwt_extended import JWTManager
+from flask_cors import CORS
 from . import config
 from .routes import auth, listings, user
 
+
 def create_app():
     app = Flask(__name__)
+
+    CORS(app, origins=['http://localhost:3000'])
 
     app.config["JWT_SECRET_KEY"] = config.JWT_SECRET_KEY
     app.config["JWT_ACCESS_TOKEN_EXPIRES"] = False
--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+const nextConfig = {
+    async rewrites() {
+      return [
+        {
+          source: '/:path*',
+          destination:
+            process.env.NODE_ENV === 'development'
+              ? 'http://127.0.0.1:5000/:path*'
+              : '/:path*',
+        },
+      ];
+    },
+  };
+  
+  module.exports = nextConfig;
