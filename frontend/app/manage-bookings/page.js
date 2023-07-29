b7296d314396f4b7e26ace68a65ffe6965ab0505
Mani Singal <mani88singal@gmail.com>
2023-07-30 00:18:41 +1000

    Fixed Bugs

--- frontend/app/manage-bookings/page.js
+++ frontend/app/manage-bookings/page.js
@@ -1,87 +1,15 @@
-'use client'
+"use client";
 
-import React, { useContext, useState } from 'react';
-import BookingContext from '@contexts/BookingContext';
+import React, { useContext, useState } from "react";
+import BookingContext from "@contexts/BookingContext";
+import BookingProvider from "@contexts/BookingProvider";
+import ManageBookings from "@components/MangeBookings";
 
 const ManageBookingPage = () => {
-  const { bookings, finishedBookings } = useContext(BookingContext);
-  const [showEditModal, setShowEditModal] = useState(false);
-  const [selectedBooking, setSelectedBooking] = useState();
-
-  // Function to handle editing the booking
-//   const handleEditBooking = (booking) => {
-//     setSelectedBooking(booking);
-//     setShowEditModal(true);
-//   };
-
-//   // Function to handle canceling the booking
-//   const handleCancelBooking = (bookingId) => {
-//     // Implement your API call or state update to cancel the booking with the provided bookingId
-//   };
-
   return (
-    <div className="flex flex-col">
-      {/* Manage Booking Section */}
-      <div className="my-8">
-        <h1 className="text-2xl font-bold mb-4">Manage Booking</h1>
-        {bookings.map((booking) => (
-          <div key={booking.id} className="border rounded p-4 mb-4">
-            <div className="flex justify-between">
-              <span className="font-bold">Booking Date:</span>
-              <span>{booking.bookingDate}</span>
-            </div>
-            {/* Display other relevant booking information */}
-            <div className="flex justify-between">
-              {/* Display other relevant booking information */}
-            </div>
-            <div className="flex mt-4">
-              <button
-                className="mr-2 bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded"
-                onClick={() => handleEditBooking(booking)}
-              >
-                Edit
-              </button>
-              <button
-                className="bg-red-500 hover:bg-red-600 text-white px-4 py-2 rounded"
-                onClick={() => handleCancelBooking(booking.id)}
-              >
-                Cancel
-              </button>
-            </div>
-          </div>
-        ))}
-      </div>
-
-      {/* Review Finished Bookings Section */}
-      <div className="my-8">
-        <h1 className="text-2xl font-bold mb-4">Review Finished Bookings</h1>
-        {finishedBookings.map((booking) => (
-          <div key={booking.id} className="border rounded p-4 mb-4">
-            <div className="flex justify-between">
-              <span className="font-bold">Booking Date:</span>
-              <span>{booking.bookingDate}</span>
-            </div>
-            {/* Display other relevant booking information */}
-            <div className="flex justify-between">
-              {/* Display other relevant booking information */}
-            </div>
-            <div className="flex mt-4">
-              {/* You can add buttons or actions related to reviewing finished bookings */}
-            </div>
-          </div>
-        ))}
-      </div>
-
-      {/* Add/Edit Booking Modal */}
-      {showEditModal && selectedBooking && (
-        <div className="fixed top-0 left-0 w-full h-full bg-gray-800 bg-opacity-50 flex items-center justify-center">
-          <div className="bg-white rounded p-6">
-            <h2 className="text-xl font-bold mb-4">Edit Booking</h2>
-            {/* Implement your edit booking form here */}
-          </div>
-        </div>
-      )}
-    </div>
+    <BookingProvider>
+      <ManageBookings />
+    </BookingProvider>
   );
 };
 
--- frontend/contexts/BookingProvider.js
+++ frontend/contexts/BookingProvider.js
@@ -1,40 +1,55 @@
-import { useState } from 'react';
-import BookingContext from '@contexts/BookingContext';
-import { getNextHour, getDate } from '@utils/utils';
-import { createContext } from 'react';
+import { useState } from "react";
+import BookingContext from "@contexts/BookingContext";
+import { getNextHour, getDate } from "@utils/utils";
+import PropTypes from "prop-types";
 
 const BookingProvider = ({ children }) => {
   const [startTime, setStartTime] = useState(getNextHour());
-  const [endTime, setEndTime] = useState(startTime + 1 > 23 ? 0 : startTime + 1);
+  const [endTime, setEndTime] = useState(
+    startTime + 1 > 23 ? 0 : startTime + 1
+  );
   const [startDate, setStartDate] = useState(getDate());
-  const [endDate, setEndDate] = useState(endTime < startTime ? new Date(new Date().setDate(new Date().getDate() + 1)) : new Date());
-  const [price, setPrice] = useState(0); 
-  const [bookings, setBookings] = useState([]); 
-  const [finishedBookings, setFinishedBookings] = useState([]); 
+  const [endDate, setEndDate] = useState(
+    endTime < startTime
+      ? new Date(new Date().setDate(new Date().getDate() + 1))
+      : new Date()
+  );
+  const [price, setPrice] = useState(0);
+  const [bookings, setBookings] = useState([]);
+  const [finishedBookings, setFinishedBookings] = useState([]);
 
-  const handleEditBooking = (booking) => {
-   
-  };
+  const handleEditBooking = (booking) => {};
 
-  const handleCancelBooking = (bookingId) => {
-    
-  };
+  const handleCancelBooking = (bookingId) => {};
 
   return (
-    <BookingContext.Provider value={{
-      startTime, setStartTime,
-      endTime, setEndTime,
-      startDate, setStartDate,
-      endDate, setEndDate,
-      price, setPrice,
-      bookings, setBookings,
-      finishedBookings, setFinishedBookings,
-      handleEditBooking,
-      handleCancelBooking,
-    }}>
+    <BookingContext.Provider
+      value={{
+        startTime,
+        setStartTime,
+        endTime,
+        setEndTime,
+        startDate,
+        setStartDate,
+        endDate,
+        setEndDate,
+        price,
+        setPrice,
+        bookings,
+        setBookings,
+        finishedBookings,
+        setFinishedBookings,
+        handleEditBooking,
+        handleCancelBooking,
+      }}
+    >
       {children}
     </BookingContext.Provider>
   );
 };
 
 export default BookingProvider;
+
+BookingProvider.propTypes = {
+  children: PropTypes.node.isRequired,
+};
