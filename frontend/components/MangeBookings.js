e6dffbca75ef404b544c0064e1ababef32724620
marianyuzu <marianyuzuninomi@gmail.com>
2023-07-30 23:27:31 +1000

    fix some bugs

--- frontend/components/MangeBookings.js
+++ frontend/components/MangeBookings.js
@@ -1,45 +1,14 @@
-
-import React from "react";
+import React, { useContext } from "react";
+import BookingContext from "@contexts/BookingContext";
 import LoginSideBar from "@components/LoginSideBar";
 
 const ManageBooking = () => {
-  const bookings = [
-    {
-      id: 1,
-      startTime: "2023-07-30 10:00 AM",
-      endTime: "2023-07-30 12:00 PM",
-      price: 100,
-    },
-    {
-      id: 2,
-      startTime: "2023-07-31 02:00 PM",
-      endTime: "2023-07-31 05:00 PM",
-      price: 150,
-    },
-  ];
-
-  const finishedBookings = [
-    {
-      id: 3,
-      startTime: "2023-07-29 08:00 AM",
-      endTime: "2023-07-29 10:00 AM",
-      price: 80,
-    },
-    {
-      id: 4,
-      startTime: "2023-07-28 11:00 AM",
-      endTime: "2023-07-28 01:00 PM",
-      price: 120,
-    },
-  ];
-
-  const handleEditBooking = (booking) => {
-    console.log("Editing booking:", booking);
-  };
-
-  const handleCancelBooking = (bookingId) => {
-    console.log("Canceling booking with ID:", bookingId);
-  };
+  const {
+    bookings,
+    finishedBookings,
+    handleEditBooking,
+    handleCancelBooking,
+  } = useContext(BookingContext);
 
   return (
     <div className="flex flex-row w-full mt-12 bg-gray-100">
@@ -50,59 +19,116 @@ const ManageBooking = () => {
         <div className="my-8">
           <h1 className="text-2xl font-bold mb-4">Current Bookings</h1>
           {bookings.map((booking) => (
-            <div key={booking.id} className="border rounded p-4 mb-4">
-              <div className="flex justify-between">
-                <span className="font-bold">Start Time:</span>
-                <span>{booking.startTime}</span>
-              </div>
-              <div className="flex justify-between">
-                <span className="font-bold">End Time:</span>
-                <span>{booking.endTime}</span>
-              </div>
-              <div className="flex justify-between">
-                <span className="font-bold">Price:</span>
-                <span>${booking.price}</span>
-              </div>
-              <div className="flex mt-4">
-                <button
-                  className="blue_btn"
-                  onClick={() => handleEditBooking(booking)}
-                >
-                  Edit
-                </button>
-                <button
-                  className="blue_btn"
-                  onClick={() => handleCancelBooking(booking.id)}
-                >
-                  Cancel
-                </button>
-              </div>
+          <div key={booking.id} className="border rounded p-4 mb-4">
+            <div className="flex justify-between">
+              <span className="font-bold">Start Time:</span>
+              <span>{booking.start_time}</span>
             </div>
-          ))}
-        </div>
-
-        <div className="my-8">
+            <div className="flex justify-between">
+              <span className="font-bold">End Time:</span>
+              <span>{booking.end_time}</span>
+            </div>
+            <div className="flex justify-between">
+              <span className="font-bold">Price:</span>
+              <span>{booking.price}</span>
+            </div>
+            <div className="flex mt-4">
+              <button
+                className="mr-2 bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded"
+                onClick={() => handleEditBooking(booking)}
+              >
+                Edit
+              </button>
+              <button
+                className="bg-red-500 hover:bg-red-600 text-white px-4 py-2 rounded"
+                onClick={() => handleCancelBooking(booking.id)}
+              >
+                Cancel
+              </button>
+            </div>
+          </div>
+        ))}
+      </div>
+      
+      <div className="my-8">
           <h1 className="text-2xl font-bold mb-4">Review Bookings</h1>
           {finishedBookings.map((booking) => (
-            <div key={booking.id} className="border rounded p-4 mb-4">
-              <div className="flex justify-between">
-                <span className="font-bold">Start Time:</span>
-                <span>{booking.startTime}</span>
-              </div>
-              <div className="flex justify-between">
-                <span className="font-bold">End Time:</span>
-                <span>{booking.endTime}</span>
-              </div>
-              <div className="flex justify-between">
-                <span className="font-bold">Price:</span>
-                <span>${booking.price}</span>
-              </div>
+          <div key={booking.id} className="border rounded p-4 mb-4">
+            <div className="flex justify-between">
+              <span className="font-bold">Start Time:</span>
+              <span>{booking.start_time}</span>
             </div>
-          ))}
+            <div className="flex justify-between">
+              <span className="font-bold">End Time:</span>
+              <span>{booking.end_time}</span>
+            </div>
+            <div className="flex justify-between">
+              <span className="font-bold">Price:</span>
+              <span>{booking.price}</span>
+            </div>
+          </div>
+        ))}
         </div>
       </div>
     </div>
+
+    // <div>
+    //   <div className="my-8">
+    //     <h1 className="text-2xl font-bold mb-4">Current Bookings</h1>
+    //     {bookings.map((booking) => (
+    //       <div key={booking.id} className="border rounded p-4 mb-4">
+    //         <div className="flex justify-between">
+    //           <span className="font-bold">Start Time:</span>
+    //           <span>{booking.start_time}</span>
+    //         </div>
+    //         <div className="flex justify-between">
+    //           <span className="font-bold">End Time:</span>
+    //           <span>{booking.end_time}</span>
+    //         </div>
+    //         <div className="flex justify-between">
+    //           <span className="font-bold">Price:</span>
+    //           <span>{booking.price}</span>
+    //         </div>
+    //         <div className="flex mt-4">
+    //           <button
+    //             className="mr-2 bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded"
+    //             onClick={() => handleEditBooking(booking)}
+    //           >
+    //             Edit
+    //           </button>
+    //           <button
+    //             className="bg-red-500 hover:bg-red-600 text-white px-4 py-2 rounded"
+    //             onClick={() => handleCancelBooking(booking.id)}
+    //           >
+    //             Cancel
+    //           </button>
+    //         </div>
+    //       </div>
+    //     ))}
+    //   </div>
+
+    //   <div className="my-8">
+    //     <h1 className="text-2xl font-bold mb-4">Review Bookings</h1>
+    //     {finishedBookings.map((booking) => (
+    //       <div key={booking.id} className="border rounded p-4 mb-4">
+    //         <div className="flex justify-between">
+    //           <span className="font-bold">Start Time:</span>
+    //           <span>{booking.start_time}</span>
+    //         </div>
+    //         <div className="flex justify-between">
+    //           <span className="font-bold">End Time:</span>
+    //           <span>{booking.end_time}</span>
+    //         </div>
+    //         <div className="flex justify-between">
+    //           <span className="font-bold">Price:</span>
+    //           <span>{booking.price}</span>
+    //         </div>
+    //       </div>
+    //     ))}
+    //   </div>
+    // </div>
   );
 };
 
 export default ManageBooking;
+
--- frontend/contexts/BookingProvider.js
+++ frontend/contexts/BookingProvider.js
@@ -15,8 +15,121 @@ const BookingProvider = ({ children }) => {
       : new Date()
   );
   const [price, setPrice] = useState(0);
-  const [bookings, setBookings] = useState([]);
-  const [finishedBookings, setFinishedBookings] = useState([]);
+  const [bookings, setBookings] = useState([
+    {
+        "_id": "60fdb79d8a32fe168d8a63f3",
+        "consumer": "60fdb79d8a32fe168d8a63f4",
+        "listing_id": "60fdb79d8a32fe168d8a63f5",
+        "start_time": "2022-01-01T00:00:00",
+        "end_time": "2022-01-23T00:00:00",
+        "price": 100.0,
+        "parent": "60fdb79d8a32fe168d8a63f6",
+        "child": "60fdb79d8a32fe168d8a63f7",
+        "exclusions": ["60fdb79d8a32fe168d8a63f8", "60fdb79d8a32fe168d8a63f9"]
+    },
+    {
+        "_id": "60fdb79d8a32fe168d8a63fa",
+        "consumer": "60fdb79d8a32fe168d8a63fb",
+        "listing_id": "60fdb79d8a32fe168d8a63fc",
+        "start_time": "2022-01-01T00:00:00",
+        "end_time": "2022-01-23T00:00:00",
+        "price": 100.0,
+        "parent": "60fdb79d8a32fe168d8a63fd",
+        "child": "60fdb79d8a32fe168d8a63fe",
+        "exclusions": ["60fdb79d8a32fe168d8a63ff", "60fdb79d8a32fe168d8a6400"]
+    },
+    {
+        "_id": "60fdb79d8a32fe168d8a6401",
+        "consumer": "60fdb79d8a32fe168d8a6402",
+        "listing_id": "60fdb79d8a32fe168d8a6403",
+        "start_time": "2022-01-01T00:00:00",
+        "end_time": "2022-01-23T00:00:00",
+        "price": 100.0,
+        "parent": "60fdb79d8a32fe168d8a6404",
+        "child": "60fdb79d8a32fe168d8a6405",
+        "exclusions": ["60fdb79d8a32fe168d8a6406", "60fdb79d8a32fe168d8a6407"]
+    },
+    {
+        "_id": "60fdb79d8a32fe168d8a6408",
+        "consumer": "60fdb79d8a32fe168d8a6409",
+        "listing_id": "60fdb79d8a32fe168d8a640a",
+        "start_time": "2022-01-01T00:00:00",
+        "end_time": "2022-01-23T00:00:00",
+        "price": 100.0,
+        "parent": "60fdb79d8a32fe168d8a640b",
+        "child": "60fdb79d8a32fe168d8a640c",
+        "exclusions": ["60fdb79d8a32fe168d8a640d", "60fdb79d8a32fe168d8a640e"]
+    },
+    {
+        "_id": "60fdb79d8a32fe168d8a640f",
+        "consumer": "60fdb79d8a32fe168d8a6410",
+        "listing_id": "60fdb79d8a32fe168d8a6411",
+        "start_time": "2022-01-01T00:00:00",
+        "end_time": "2022-01-23T00:00:00",
+        "price": 100.0,
+        "parent": "60fdb79d8a32fe168d8a6412",
+        "child": "60fdb79d8a32fe168d8a6413",
+        "exclusions": ["60fdb79d8a32fe168d8a6414", "60fdb79d8a32fe168d8a6415"]
+    }
+]);
+  const [finishedBookings, setFinishedBookings] = useState([
+    {
+      "_id": "60fdb79d8a32fe168d8a63f3",
+      "consumer": "60fdb79d8a32fe168d8a63f4",
+      "listing_id": "60fdb79d8a32fe168d8a63f5",
+      "start_time": "2022-01-01T00:00:00",
+      "end_time": "2022-01-23T00:00:00",
+      "price": 100.0,
+      "parent": "60fdb79d8a32fe168d8a63f6",
+      "child": "60fdb79d8a32fe168d8a63f7",
+      "exclusions": ["60fdb79d8a32fe168d8a63f8", "60fdb79d8a32fe168d8a63f9"]
+    },
+    {
+      "_id": "60fdb79d8a32fe168d8a63fa",
+      "consumer": "60fdb79d8a32fe168d8a63fb",
+      "listing_id": "60fdb79d8a32fe168d8a63fc",
+      "start_time": "2022-01-01T00:00:00",
+      "end_time": "2022-01-23T00:00:00",
+      "price": 100.0,
+      "parent": "60fdb79d8a32fe168d8a63fd",
+      "child": "60fdb79d8a32fe168d8a63fe",
+      "exclusions": ["60fdb79d8a32fe168d8a63ff", "60fdb79d8a32fe168d8a6400"]
+    },
+    {
+      "_id": "60fdb79d8a32fe168d8a6401",
+      "consumer": "60fdb79d8a32fe168d8a6402",
+      "listing_id": "60fdb79d8a32fe168d8a6403",
+      "start_time": "2022-01-01T00:00:00",
+      "end_time": "2022-01-23T00:00:00",
+      "price": 100.0,
+      "parent": "60fdb79d8a32fe168d8a6404",
+      "child": "60fdb79d8a32fe168d8a6405",
+      "exclusions": ["60fdb79d8a32fe168d8a6406", "60fdb79d8a32fe168d8a6407"]
+    },
+    {
+      "_id": "60fdb79d8a32fe168d8a6408",
+      "consumer": "60fdb79d8a32fe168d8a6409",
+      "listing_id": "60fdb79d8a32fe168d8a640a",
+      "start_time": "2022-01-01T00:00:00",
+      "end_time": "2022-01-23T00:00:00",
+      "price": 100.0,
+      "parent": "60fdb79d8a32fe168d8a640b",
+      "child": "60fdb79d8a32fe168d8a640c",
+      "exclusions": ["60fdb79d8a32fe168d8a640d", "60fdb79d8a32fe168d8a640e"]
+    },
+    {
+      "_id": "60fdb79d8a32fe168d8a640f",
+      "consumer": "60fdb79d8a32fe168d8a6410",
+      "listing_id": "60fdb79d8a32fe168d8a6411",
+      "start_time": "2022-01-01T00:00:00",
+      "end_time": "2022-01-23T00:00:00",
+      "price": 100.0,
+      "parent": "60fdb79d8a32fe168d8a6412",
+      "child": "60fdb79d8a32fe168d8a6413",
+      "exclusions": ["60fdb79d8a32fe168d8a6414", "60fdb79d8a32fe168d8a6415"]
+    }
+  ]
+  );
 
   const handleEditBooking = (booking) => {};
 
