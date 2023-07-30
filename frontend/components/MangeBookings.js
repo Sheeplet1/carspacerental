2b296bf53c7891e00fbbd54fba45ea938343e57d
marianyuzu <marianyuzuninomi@gmail.com>
2023-07-30 15:27:35 +1000

    make some changes

--- frontend/components/MangeBookings.js
+++ frontend/components/MangeBookings.js
@@ -1,84 +1,108 @@
-"use client";
 
-import React, { useContext, useState } from "react";
-import BookingContext from "@contexts/BookingContext";
+import React from "react";
+import LoginSideBar from "@components/LoginSideBar";
 
-const ManageBookingPage = () => {
-  const { bookings, finishedBookings } = useContext(BookingContext);
-  const [showEditModal, setShowEditModal] = useState(false);
-  const [selectedBooking, setSelectedBooking] = useState();
+const ManageBooking = () => {
+  const bookings = [
+    {
+      id: 1,
+      startTime: "2023-07-30 10:00 AM",
+      endTime: "2023-07-30 12:00 PM",
+      price: 100,
+    },
+    {
+      id: 2,
+      startTime: "2023-07-31 02:00 PM",
+      endTime: "2023-07-31 05:00 PM",
+      price: 150,
+    },
+  ];
+
+  const finishedBookings = [
+    {
+      id: 3,
+      startTime: "2023-07-29 08:00 AM",
+      endTime: "2023-07-29 10:00 AM",
+      price: 80,
+    },
+    {
+      id: 4,
+      startTime: "2023-07-28 11:00 AM",
+      endTime: "2023-07-28 01:00 PM",
+      price: 120,
+    },
+  ];
 
   const handleEditBooking = (booking) => {
-    setSelectedBooking(booking);
-    setShowEditModal(true);
+    console.log("Editing booking:", booking);
   };
 
-  const handleCancelBooking = (bookingId) => {};
+  const handleCancelBooking = (bookingId) => {
+    console.log("Canceling booking with ID:", bookingId);
+  };
 
   return (
-    <div className="flex flex-col">
-      {/* Manage Booking Section */}
-      <div className="my-8">
-        <h1 className="text-2xl font-bold mb-4">Manage Booking</h1>
-        {bookings.map((booking) => (
-          <div key={booking.id} className="border rounded p-4 mb-4">
-            <div className="flex justify-between">
-              <span className="font-bold">Booking Date:</span>
-              <span>{booking.bookingDate}</span>
-            </div>
-            {/* Display other relevant booking information */}
-            <div className="flex justify-between">
-              {/* Display other relevant booking information */}
-            </div>
-            <div className="flex mt-4">
-              <button
-                className="mr-2 bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded"
-                onClick={() => handleEditBooking(booking)}
-              >
-                Edit
-              </button>
-              <button
-                className="bg-red-500 hover:bg-red-600 text-white px-4 py-2 rounded"
-                onClick={() => handleCancelBooking(booking.id)}
-              >
-                Cancel
-              </button>
-            </div>
-          </div>
-        ))}
+    <div className="flex flex-row w-full mt-12 bg-gray-100">
+      <div className="w-1/3 rounded-lg p-5">
+        <LoginSideBar />
       </div>
-
-      {/* Review Finished Bookings Section */}
-      <div className="my-8">
-        <h1 className="text-2xl font-bold mb-4">Review Finished Bookings</h1>
-        {finishedBookings.map((booking) => (
-          <div key={booking.id} className="border rounded p-4 mb-4">
-            <div className="flex justify-between">
-              <span className="font-bold">Booking Date:</span>
-              <span>{booking.bookingDate}</span>
+      <div className="flex flex-col w-full ml-5 p-5 bg-white shadow-md rounded-lg">
+        <div className="my-8">
+          <h1 className="text-2xl font-bold mb-4">Current Bookings</h1>
+          {bookings.map((booking) => (
+            <div key={booking.id} className="border rounded p-4 mb-4">
+              <div className="flex justify-between">
+                <span className="font-bold">Start Time:</span>
+                <span>{booking.startTime}</span>
+              </div>
+              <div className="flex justify-between">
+                <span className="font-bold">End Time:</span>
+                <span>{booking.endTime}</span>
+              </div>
+              <div className="flex justify-between">
+                <span className="font-bold">Price:</span>
+                <span>${booking.price}</span>
+              </div>
+              <div className="flex mt-4">
+                <button
+                  className="blue_btn"
+                  onClick={() => handleEditBooking(booking)}
+                >
+                  Edit
+                </button>
+                <button
+                  className="blue_btn"
+                  onClick={() => handleCancelBooking(booking.id)}
+                >
+                  Cancel
+                </button>
+              </div>
             </div>
-            {/* Display other relevant booking information */}
-            <div className="flex justify-between">
-              {/* Display other relevant booking information */}
-            </div>
-            <div className="flex mt-4">
-              {/* You can add buttons or actions related to reviewing finished bookings */}
-            </div>
-          </div>
-        ))}
-      </div>
-
-      {/* Add/Edit Booking Modal */}
-      {showEditModal && selectedBooking && (
-        <div className="fixed top-0 left-0 w-full h-full bg-gray-800 bg-opacity-50 flex items-center justify-center">
-          <div className="bg-white rounded p-6">
-            <h2 className="text-xl font-bold mb-4">Edit Booking</h2>
-            {/* Implement your edit booking form here */}
-          </div>
+          ))}
         </div>
-      )}
+
+        <div className="my-8">
+          <h1 className="text-2xl font-bold mb-4">Review Bookings</h1>
+          {finishedBookings.map((booking) => (
+            <div key={booking.id} className="border rounded p-4 mb-4">
+              <div className="flex justify-between">
+                <span className="font-bold">Start Time:</span>
+                <span>{booking.startTime}</span>
+              </div>
+              <div className="flex justify-between">
+                <span className="font-bold">End Time:</span>
+                <span>{booking.endTime}</span>
+              </div>
+              <div className="flex justify-between">
+                <span className="font-bold">Price:</span>
+                <span>${booking.price}</span>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
 
-export default ManageBookingPage;
+export default ManageBooking;
