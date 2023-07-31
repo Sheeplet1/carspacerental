ec64940e53a8b5082d0c9042489279ac5999a160
Careen Hakim <careenhakim@Careens-MacBook-Pro.local>
2023-07-31 22:54:00 +1000

    Done with Wallet

--- frontend/app/wallet/page.js
+++ frontend/app/wallet/page.js
@@ -1,34 +1,48 @@
-'use client'
+"use client";
 
-import React from 'react'
-import LoginSidebar from '@components/LoginSideBar';
-import Link from 'next/link'
-import { useState } from 'react';
-import SearchBar from '@components/SearchBar';
-import { useRouter } from 'next/navigation';
+import Sidebar from "@components/Sidebar";
+import { useState } from "react";
+import LoginSidebar from "@components/LoginSideBar";
+import { useUser } from "@contexts/UserProvider";
+import WithdrawModal from "@components/WithdrawModal";
+import ConfirmModal from "@components/ConfirmModal";
 
 const Wallet = () => {
+  const { user } = useUser();
+  const [showWithdrawModal, setShowWithdrawModal] = useState(false);
+  const [showConfirmModal, setShowConfirmModal] = useState(false);
+
+  const setConfirmModalHandler = () => {
+    setShowConfirmModal(true);
+  };
 
   return (
     <div className='flex flex-row w-full justify-between mt-12'>
-      <div className='w-1/3'>
-        <LoginSidebar />
-      </div>
+      <div className='w-1/3'>{user ? <LoginSidebar /> : <Sidebar />}</div>
       <div className='flex flex-col w-2/3'>
-        <h1 className='heading_text'>
-          Wallet
-        </h1>
+        <h1 className='heading_text'>Wallet</h1>
 
-        <div className="border border-blue-300 rounded p-4">
-
+        <div className='border bg-custom-blue rounded p-4 mt-2'>
+          <p className='text-gray-500 text-xs'>Current balance:</p>
+          <p>$xxx.xx</p>
         </div>
 
-        <h1 href='#' className='heading_text hover:underline'>
-          Recent transactions
-        </h1>
+        <WithdrawModal
+          showWithdrawModal={showWithdrawModal}
+          setShowWithdrawModal={setShowWithdrawModal}
+          setConfirmModalHandler={setConfirmModalHandler}
+        />
+        <ConfirmModal
+          showConfirmModal={showConfirmModal}
+          setShowConfirmModal={setShowConfirmModal}
+        />
+
+        <h1 className='heading_text mt-20'>Recent transactions</h1>
+        <hr className='border-t-2 border-gray-300 my-4'/>
+        <a className='flex justify-center text-gray-400 hover:underline'>Show all transactions</a>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default Wallet
--- frontend/components/WithdrawModal.js
+++ frontend/components/WithdrawModal.js
@@ -5,20 +5,21 @@ import { useState, useRef, useEffect } from "react";
 import { useUser } from "@contexts/UserProvider";
 import PaymentDetailsModal from "@components/PaymentDetailsModal";
 import PropTypes from "prop-types";
+import Select from "react-select";
+import { makeRequest } from "@utils/utils";
 
 const WithdrawModal = ({
   showWithdrawModal,
   setShowWithdrawModal,
-  btnTitle,
-  modalHeader,
-  withdraw_payment_details,
-  withdraw_amount,
+  setConfirmModalHandler,
+  payment_details,
+  amount,
 }) => {
   const [showPaymentDetailsModal, setShowPaymentDetailsModal] = useState(false);
-  const [withdrawPaymentDetails, setWithdrawPaymentDetails] = useState(withdraw_payment_details);
-  const [withdrawAmount, setWithdrawAmount] = useState(withdraw_amount);
+  const [withdrawPaymentDetails, setWithdrawPaymentDetails] = useState(payment_details);
+  const [withdrawAmount, setWithdrawAmount] = useState(amount);
   const [withdrawAmountError, setWithdrawAmountError] = useState("");
-  const { user, updateUser } = useUser();
+  const { user } = useUser();
 
   const paymentMethodOptions = user.payment_details.map((payment) => ({
     value: payment.card_number,
@@ -26,9 +27,9 @@ const WithdrawModal = ({
   }));
 
   useEffect(() => {
-    setWithdrawPaymentDetails(withdraw_payment_details);
-    setWithdrawAmount(withdraw_amount);
-  }, [withdraw_payment_details, amount, showWithdrawModal]);
+    setWithdrawPaymentDetails(payment_details);
+    setWithdrawAmount(amount);
+  }, [payment_details, amount, showWithdrawModal]);
 
   const ref = useRef(null);
 
@@ -36,36 +37,41 @@ const WithdrawModal = ({
     ref.current = document.body;
   }, []);
 
-  const clickNext = async () => {
-
+  const validateConfirm = async () => {
     const isAmountExist = !!withdrawAmount;
     setWithdrawAmountError(isAmountExist ? "" : "This field is required");
 
     if (isAmountExist) {
-        const newWithdrawDetail = {
-          withdraw_payment_details: withdrawPaymentDetails,
-          withdraw_amount: withdrawAmount,
-        };
+      const body = {
+        amt: parseFloat(withdrawAmount)
+      }
+      const response = await makeRequest("/withdraw", "POST", body);
+      if (response.error) {
+        throw new Error(response.error);
+      } else {
+        closeWithdrawModal();
+        setConfirmModalHandler();
+      }
     }
   };
 
-  const closeModal = () => {
-
+  const closeWithdrawModal = () => {
+    setWithdrawAmountError("");
+    setWithdrawAmount("");
+    setShowWithdrawModal(false);
   }
 
   return (
     <div>
       <Button
         className='bg-custom-orange hover:bg-custom-orange-dark text-white'
-        onClick={() => setShowWithdrawModal(true)}>
-          {btnTitle}
-      </Button>
+        onClick={() => setShowWithdrawModal(true)}>Withdraw</Button>
 
       <Modal
         show={showWithdrawModal}
-        onClose={closeModal}
+        onClose={closeWithdrawModal}
         root={ref.current}>
-        <Modal.Header className="bg-custom-orange text-white">{modalHeader}</Modal.Header>
+        <Modal.Header className="bg-custom-orange text-white">Withdraw Details</Modal.Header>
 
         <Modal.Body>
           <div>
@@ -88,9 +94,10 @@ const WithdrawModal = ({
           </div>
 
           <div>
-              <div className="mb-2 block">
-                <Label htmlFor="small" value="Withdraw Amount" />
-              </div>
+            <div className="mb-2 block">
+              <Label htmlFor="small" value="Withdraw Amount" />
+            </div>
+            <div className="relative">
               <input
                 className="w-full border-2 border-gray-300 rounded-3xl p-2 mt-2 mb-4"
                 type="text"
@@ -98,23 +105,24 @@ const WithdrawModal = ({
                 value={withdrawAmount}
                 onChange={(e) => setWithdrawAmount(e.target.value)}
               />
-              <p className="error_text">{setWithdrawAmountError}</p>
             </div>
+            <p className="error_text">{withdrawAmountError}</p>
+          </div>
         </Modal.Body>
 
         <Modal.Footer>
-        <div className="flex flex-row justify-end space-x-4">
+          <div className="flex flex-row justify-end space-x-4">
             <Button
               className="bg-custom-orange hover:bg-custom-orange-dark text-white"
-              onClick={closeModal}
+              onClick={closeWithdrawModal}
             >
               Cancel
             </Button>
             <Button
               className="bg-custom-orange hover:bg-custom-orange-dark text-white"
-              onClick={clickNext}
+              onClick={validateConfirm}
             >
-              Next
+              Confirm
             </Button>
           </div>
         </Modal.Footer>
@@ -125,11 +133,10 @@ const WithdrawModal = ({
 
 export default WithdrawModal
 
-WithdrawModal.PropTypes = {
+WithdrawModal.propTypes = {
   showWithdrawModal: PropTypes.bool.isRequired,
   setShowWithdrawModal: PropTypes.func.isRequired,
-  btnTitle: PropTypes.string.isRequired,
-  modalHeader: PropTypes.string.isRequired,
-  withdraw_payment_details: PropTypes.string.isRequired,
-  withdraw_amount: PropTypes.number.isRequired,
+  setConfirmModalHandler: PropTypes.func.isRequired,
+  payment_details: PropTypes.string.isRequired,
+  amount: PropTypes.number.isRequired,
 };
